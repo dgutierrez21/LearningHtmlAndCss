c9b8ae3f58c545b10b31e1cd941eb9cd0fd40a4e
Dixon G <dxxn0101@gmail.com>
2022-08-21 22:21:29 -0600

    improving script for the section calculate

--- css/CSSpractice/ResponsiveGymWebsiteDesign/js/main.js
+++ css/CSSpractice/ResponsiveGymWebsiteDesign/js/main.js
@@ -46,10 +46,22 @@ window.addEventListener("scroll", scrollHeader);
 const calculateForm = document.querySelector("#calculate-form"),
   calculateCm = document.querySelector("#calculate-cm"),
   calculateKg = document.querySelector("#calculate-kg"),
+  calculateButton = document.querySelector("#calculate-button"),
   calculateMessage = document.querySelector("#calculate-message");
 
 const calculateBmi = (event) => {
   event.preventDefault();
+
+  // Funtion deleteMessage
+  const deleteMessage = (seconds) => {
+    calculateButton.disabled = true;
+    calculateButton.style.opacity = 0.5;
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+      calculateButton.disabled = false;
+      calculateButton.style.opacity = 1;
+    }, seconds);
+  };
 
   // Check if the fields have a value
   if (calculateCm.value === "" || calculateKg.value === "") {
@@ -61,10 +73,12 @@ const calculateBmi = (event) => {
     calculateMessage.textContent = "Fill in the Height and Weight";
 
     //delete the message after three seconds
-    setTimeout(() => {
-      calculateMessage.textContent = "";
-    }, 3000);
+    deleteMessage(3000);
   } else {
+    // add color and display message
+    calculateMessage.classList.remove("color-red");
+    calculateMessage.classList.add("color-green");
+
     // BMI Formula
     // How is BMI calculated? With the metric system, the formula for BMI is weight in kilograms divided by height in meters squared.
     const cm = calculateCm.value / 100,
@@ -77,50 +91,30 @@ const calculateBmi = (event) => {
       // add color and display message
       calculateMessage.classList.remove("color-green");
       calculateMessage.classList.add("color-red");
+
       calculateMessage.textContent = `Enter a valid value`;
     } else if (bmi < 18.5) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.textContent = `Your BMI is ${bmi} and your are skinny`;
     } else if (bmi < 25) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.textContent = `Your BMI is ${bmi} and your are healthy`;
     } else if (bmi < 30) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.textContent = `Your BMI is ${bmi} and your are pre-obese(risk)`;
     } else if (bmi < 35) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.textContent = `Your BMI is ${bmi} and your are obese type I (moderate risk)`;
     } else if (bmi < 40) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.textContent = `Your BMI is ${bmi} and your are obese type II obese type II (severe risk)`;
-    } else if (bmi > 40 && bmi <= 185.5) {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
-      calculateMessage.textContent = `Your BMI is ${bmi} and your are obese type III (very severe risk)`;
+    } else if (bmiPrecise > 40 && bmiPrecise < 185.6) {
+      calculateMessage.textContent = `Your BMI is ${bmiPrecise.toFixed(
+        1
+      )} and your are obese type III (very severe risk)`;
     } else {
-      // add color and display message
-      calculateMessage.classList.remove("color-red");
-      calculateMessage.classList.add("color-green");
       calculateMessage.innerHTML = `You have broken the Guinness World Record for the heaviest person on record with a bmi of ${bmiPrecise.toFixed(
         1
       )} <a href="https://en.wikipedia.org/wiki/List_of_heaviest_people" target="_blank" class="calculate__message__link">see here</a>`;
     }
 
-    //remove message
-    setTimeout(() => {
-      calculateMessage.textContent = "";
-    }, 4000);
+    //delete the message after five seconds
+    deleteMessage(5000);
   }
 };
 
